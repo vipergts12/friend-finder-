1ba2bac3e0a0d82bd6aebfcfce201ae0c2229082
Oscar Enriquez <macair@Oscars-Air.home>
2019-11-17 21:29:54 -0500

    api routes changed

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,46 @@
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/home.html"));
-});
+var path = require('path');
 
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/survey.html"));
-});
+var friends = require('../data/friends.js');
 
-// Displays all characters
-app.get("/api/characters", function (req, res) {
-    return res.json(Friends);
+module.exports = function (app) {
+    // console.log('___ENTER apiRoutes.js___');
+
+    // Total list of friend entries
+    app.get('/api/friends', function (req, res) {
+        var userInput = req.body
+        var userResponses = userInput.scores;
+
+    var matchName = '';
+    var matchImage = '';
+    var totalDifference = 10000; // Make the initial value big for comparison
+
+    // Examine all existing friends in the list
+    for (var i = 0; i < friends.length; i++) {
+        // console.log('friend = ' + JSON.stringify(friends[i]));
+
+        // Compute differenes for each question
+        var diff = 0;
+        for (var j = 0; j < userResponses.length; j++) {
+            diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+        }
+        // console.log('diff = ' + diff);
+
+        // If lowest difference, record the friend match
+        if (diff < totalDifference) {
+            // console.log('Closest match found = ' + diff);
+            // console.log('Friend name = ' + friends[i].name);
+            // console.log('Friend image = ' + friends[i].photo);
+
+            totalDifference = diff;
+            matchName = friends[i].name;
+            matchImage = friends[i].photo;
+        }
+    }
+
+    // Add new user
+    friends.push(userInput);
+
+    // Send appropriate response
+    res.json({ status: 'OK', matchName: matchName, matchImage: matchImage });
 });
+};
